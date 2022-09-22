a66ac5928f994ddf6cc8cc6b853a810652c9bb88
Saad <tmsaadrayhan@gmail.com>
2022-09-22 16:25:35 +0600

    c

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 const phoneMinus= document.getElementById("phoneMinus");
 const phoneInput= document.getElementById("phoneInput");
 const phonePlus= document.getElementById("phonePlus");
+const phoneTotal= document.getElementById("phoneTotal");
 const caseMinus= document.getElementById("caseMinus");
 const caseInput= document.getElementById("caseInput");
 const casePlus= document.getElementById("casePlus");
+const caseTotal= document.getElementById("caseTotal");
 
-handleNumber(phoneMinus, phoneInput, -1,-1219);
-handleNumber(phonePlus, phoneInput, 1, 1219);
-handleNumber(caseMinus, caseInput, -1, -59);
-handleNumber(casePlus, caseInput, 1, 59);
-function handleNumber(countId, inputId, multiplier, countAmount){
+handleNumber(phoneMinus, phoneTotal, phoneInput, -1,1219);
+handleNumber(phonePlus, phoneTotal, phoneInput, 1, 1219);
+handleNumber(caseMinus, caseTotal, caseInput, -1, 59);
+handleNumber(casePlus, caseTotal, caseInput, 1, 59);
+function handleNumber(countId, itemTotal, inputId, multiplier, countAmount){
     countId.addEventListener("click", function(){
         inputId.value= (parseFloat(inputId.value))+(multiplier);
         const price= parseFloat(document.getElementById("amount").innerText);
+        itemTotal.innerText= countAmount*(parseFloat(inputId.value));
         document.getElementById("amount").innerText= price + countAmount;
-        document.getElementById("vat").innerText= (price + countAmount)*2/100;
-        document.getElementById("total").innerText= (price + countAmount) + (price + countAmount)*2/100;
+        document.getElementById("vat").innerText= (price + multiplier*countAmount)*2/100;
+        document.getElementById("total").innerText= (price + countAmount) + (price + multiplier*countAmount)*2/100;
         const phoneDisplay= document.getElementById("phoneDisplay");
         const caseDisplay= document.getElementById("caseDisplay");
         display(phoneInput, phoneDisplay);
